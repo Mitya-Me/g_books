a9e741dae91a94be1c4fb8101dd55db02686f5bb
mitya_me <sengokusky@gmail.com>
2021-10-31 18:09:55 +0300

    App wrapped in Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
+import { store } from './store';
+
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
