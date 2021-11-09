6f5b83ef880332d94d291bbabcf6f3187126d5d2
mitya_me <sengokusky@gmail.com>
2021-11-09 09:53:54 +0300

    add arrays: categories and sortOptions

--- src/base.js
+++ src/base.js
@@ -1,3 +1,9 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const API_URL = 'https://www.googleapis.com/books/v1/volumes?';
 
+export const categoriesOptions = [
+    'All', 'Art', 'Biography', 'Computers',
+    'History', 'Medical', 'Poetry'
+]
+
+export const sortOptions = ['relevance', 'newest']
